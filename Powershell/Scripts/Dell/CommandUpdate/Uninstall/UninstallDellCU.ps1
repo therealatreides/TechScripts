c7068fb770f1fdc9a7fc507290898a5a5ea635c7
Paul Atreides <47747094+therealatreides@users.noreply.github.com>
2022-03-13 13:35:26 -0400

    Cleanup and Fixes
    + Moved and Renamed some items for organization
    * Fixed a typo in SA Uninstall
    + Added uninstaller for Dell Command Update tool

--- Powershell/Scripts/Dell/CommandUpdate/Uninstall/UninstallDellCU.ps1
+++ Powershell/Scripts/Dell/CommandUpdate/Uninstall/UninstallDellCU.ps1
@@ -1,10 +1,10 @@
 <#
 .DESCRIPTION
-    This script will find and uninstall matches containging SupportAssist in the application name.
+    This script will find and uninstall matches containging Command | Update in the application name.
 .NOTES
     Version:            1.0
     Author:             Scott E. Royalty
-    Last Modified Date: 11/15/2021
+    Last Modified Date: 03/13/2022
 #>
 
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
@@ -12,12 +12,12 @@ If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]:
     Exit
 }
 
-$SAVer = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall  |
+$CUVer = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*, HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*  |
     Get-ItemProperty |
-        Where-Object {$_.DisplayName -match "SupportAssist" } |
+        Where-Object {$_.DisplayName -match "Command | Update" } |
             Select-Object -Property DisplayVersion, UninstallString, PSChildName
 
-ForEach ($ver in $SAVer) {
+ForEach ($ver in $CUVer) {
     If ($ver.UninstallString) {
         $uninst = $ver.UninstallString
         & cmd /c $uninst /quiet /norestart
