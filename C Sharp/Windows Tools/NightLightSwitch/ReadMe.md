b9d5e0bb53b39c68f133dfefd548e65b211850b5
Paul Atreides <47747094+therealatreides@users.noreply.github.com>
2022-08-01 10:40:44 -0400

    Adding NightLightSwitch project

---
+++ C Sharp/Windows Tools/NightLightSwitch/ReadMe.md
@@ -0,0 +1,25 @@
+# Night Light Switch
+
+This is a little tool to toggle night light mode in Windows on/off using a 3rd party application.
+
+The reason for a tool, is to automate the process in a batch, powershell, or scheduled task in case you have
+a need for it to be toggled without user interaction.
+
+## Usage
+
+### NightLightSwitch.exe (options)
+
+#### Options:
+* on, 1, true   : Enable Night light mode
+* off, 0, false : Disable Night light mode
+
+## Donations
+To support this project, you can make a donation to the current maintainer:
+
+### Paypal:
+
+[![Paypal](https://github.com/therealatreides/TechScripts/blob/main/ImageRepository/paypal_btn_donateCC_LG_1.gif)](https://www.paypal.com/donate/?hosted_button_id=E6BPVFVTQ6TKU)
+
+### Cashapp: $MuadDibttv
+
+<img src="https://github.com/therealatreides/TechScripts/blob/main/ImageRepository/cashapp_qr.png" alt="Cashapp QR for $MuadDibttv" width="265px">
---
+++ Powershell/Scripts/Browsers/DisableNotifications.ps1
@@ -0,0 +1,17 @@
+<#
+.DESCRIPTION
+    This script will check for and disable the annoying browser notifications for Chome and Edge Chromium at the Current User level.
+.NOTES
+    Version:            1.0
+    Author:             Scott E. Royalty
+    Last Modified Date: 8/1/2022
+#>
+if ((Test-Path -LiteralPath "Registry::\HKEY_CURRENT_USER\Software\Policies\Google\Chrome") -ne $true) {
+    New-Item "Registry::\HKEY_CURRENT_USER\Software\Policies\Google\Chrome" -force -ea SilentlyContinue
+}
+New-ItemProperty -LiteralPath 'Registry::\HKEY_CURRENT_USER\Software\Policies\Google\Chrome' -Name 'DefaultNotificationsSetting' -Value '2' -PropertyType DWord
+
+if ((Test-Path -LiteralPath "Registry::\HKEY_CURRENT_USER\Software\Policies\Microsoft\Edge") -ne $true) {
+    New-Item "Registry::\HKEY_CURRENT_USER\Software\Policies\Microsoft\Edge" -force -ea SilentlyContinue
+}
+New-ItemProperty -LiteralPath 'Registry::\HKEY_CURRENT_USER\Software\Policies\Microsoft\Edge' -Name 'DefaultNotificationsSetting' -Value '2' -PropertyType DWord
