9ebc49271ff50b4c6676eb7579f55ad8439bd9c6
Paul Atreides <47747094+therealatreides@users.noreply.github.com>
2022-08-01 00:58:18 -0400

    Generic Splash Screen App

---
+++ C Sharp/Generic/SplashScreen/SplashIt/SplashIt/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SplashIt
+{
+    static class Program
+    {
+        private static string pageURL = @"\\localhost\share\index.htm";
+        private static string app2Mon = "kix32.exe";
+        private static int width = 630;
+        private static int height = 480;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // To check the length of 
+            // Command line arguments  
+            if (args.Length == 0 || args.Contains("--help", StringComparer.OrdinalIgnoreCase) || args.Contains("-h", StringComparer.OrdinalIgnoreCase))
+            {
+                string msgText = "Help Requested by the Admin:"+Environment.NewLine+ Environment.NewLine;
+                msgText += "SplashIt <flags> <options>" + Environment.NewLine;
+                msgText += Environment.NewLine;
+                msgText += "Flags (optional):" + Environment.NewLine;
+                msgText += "--help, -h      : View this message" + Environment.NewLine;
+                msgText += Environment.NewLine;
+                msgText += "Options (required):" + Environment.NewLine;
+                msgText += "<process>       : Process name to monitor for" + Environment.NewLine;
+                msgText += "<splash file>   : HTML based file to use as a loading/splash screen" + Environment.NewLine;
+                msgText += "<width>         : Width of the window" + Environment.NewLine;
+                msgText += "<height>        : Height of the window" + Environment.NewLine;
+                msgText += Environment.NewLine;
+                msgText += "Examples:" + Environment.NewLine;
+                msgText += @"Z:\SplashIt.exe kix32.exe Z:\Splash.html 630 480" + Environment.NewLine;
+                msgText += "Z:\\SplashIt.exe kix32.exe \"Z:\\My Splashes\\Splash.html\" 630 480" + Environment.NewLine;
+
+                MessageBox.Show(msgText);
+                Environment.Exit(0);
+            }
+            else if (args.Length < 4)
+            {
+                string msgText = "Arguments missing. Please use --help or no options to see help." + Environment.NewLine;
+                MessageBox.Show(msgText);
+                Environment.Exit(0);
+            }
+            else
+            {
+                pageURL = args[0];
+                app2Mon = args[1];
+                width = Convert.ToInt32(args[2]);
+                height = Convert.ToInt32(args[3]);
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new SplashForm(pageURL, app2Mon, width, height));
+        }
+    }
+}
---
+++ C Sharp/Generic/SplashScreen/SplashIt/SplashIt/SplashForm.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SplashIt
+{
+    public partial class SplashForm : Form
+    {
+        private string splashUrl = string.Empty;
+        private string splashApp = string.Empty;
+        private int splashWidth = 0;
+        private int splashHeight = 0;
+
+        public SplashForm(string app2Mon, string pageUrl, int swidth, int sheight)
+        {
+            splashApp = app2Mon;
+            splashUrl = pageUrl;
+            splashWidth = swidth;
+            splashHeight = sheight;
+            InitializeComponent();
+        }
+
+        private void SplashForm_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(splashWidth, splashHeight);
+            browserControl.Navigate(this.splashUrl);
+
+            BackgroundWorker worker = new BackgroundWorker();
+            worker.DoWork += Worker_DoWork;
+            worker.ProgressChanged += Worker_ProgressChanged;
+            worker.WorkerReportsProgress = false;
+            worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
+
+            worker.RunWorkerAsync(this.splashApp);
+        }
+
+        private void SplashForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Check to see if the application is still running. If so, don't allow it to close. Otherwise, do nothing.
+            // Remember, the Process Name is not the filename
+            bool isRunning = Process.GetProcessesByName(splashApp).Length > 0;
+            if (!isRunning)
+            {
+                // App being monitored is not running anymore. So do any SplashScreen Cleanup and go on so it can close out.
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //DoWork is the most important event. It is where the actual calculations are done.
+            string appName = (string)e.Argument;
+            bool isRunning = Process.GetProcessesByName(appName).Length > 0;
+            do
+            {
+                Thread.Sleep(1250);
+                isRunning = Process.GetProcessesByName(appName).Length > 0;
+            }
+            while (isRunning);
+        }
+
+        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // Nothing to do here for this one.
+        }
+
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //This method is optional but very useful. 
+            //It is called once Worker_DoWork has finished.
+            this.Close();
+        }
+    }
+}
