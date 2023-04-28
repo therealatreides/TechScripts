13b8aed30a2f857711623bb13fe8ab6648d28592
Paul Atreides <47747094+therealatreides@users.noreply.github.com>
2023-04-28 07:30:20 -0400

    Add Install-O365Modules.ps1
    Add Install-O365Modules.ps1 for installing all current Office 365 modules.
    Installs pre-release of Exchange module for MFA and non-basic auth support.

---
+++ Powershell/Scripts/Windows/Powershell/Install-O365Modules.ps1
@@ -0,0 +1,32 @@
+<#
+.SYNOPSIS
+    Installs all of the Office 365 related Powershell Modules.
+.DESCRIPTION
+    Installs all of the Office 365 related Powershell Modules.
+.EXAMPLE
+    Install-O365Modules.ps1
+.NOTES
+    Version:            1.0
+    Author:             Scott E. Royalty
+    Last Modified Date: 4/28/2023
+#>
+
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`" $PSCommandArgs" -Verb RunAs
+    Exit
+}
+
+Write-Host "Installing MSonline..."
+Install-Module -Name MSOnline -Force
+
+Write-Host "Installing AzureAD and Azure Graph modules..."
+Install-Module -Name AzureAD -Force
+
+Write-Host "Installing Exchange Online modules..."
+Install-Module -Name ExchangeOnlineManagement -Force -AllowPrerelease
+
+Write-Host "Installing SharePoint Online modules..."
+Install-Module -Name Microsoft.Online.SharePoint.PowerShell -Force
+
+Write-Host "Installing Teams modules..."
+Install-Module -Name MicrosoftTeams -Force
